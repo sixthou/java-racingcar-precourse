b73eafc2362d73a2d72c61a8cf22397eea70f06f
sixthou <justyuk@naver.com>
2022-10-10 05:19:30 +0900

    feat : ui 영역 구현

---
+++ src/main/java/racingcar/ui/Output.java
@@ -0,0 +1,13 @@
+package racingcar.ui;
+
+import java.util.List;
+
+public interface Output {
+
+    void printResultNotice();
+
+    void printMovingResults(String carsMovingResults);
+
+    void printWinners(List<String> names);
+
+}
---
+++ src/main/java/racingcar/ui/StandardOutput.java
@@ -0,0 +1,29 @@
+package racingcar.ui;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class StandardOutput implements Output {
+
+    private static final StringJoiner stringJoiner = new StringJoiner(", ");
+
+    @Override
+    public void printResultNotice() {
+        System.out.println("실행 결과");
+    }
+
+    @Override
+    public void printMovingResults(String carsMovingResults) {
+        System.out.println(carsMovingResults);
+    }
+
+    @Override
+    public void printWinners(List<String> names) {
+        stringJoiner.setEmptyValue("");
+        for (String name : names) {
+            stringJoiner.add(name);
+        }
+        System.out.println("최종 우승자 : " + stringJoiner);
+    }
+
+}
